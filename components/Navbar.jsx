b855273391109e6da383d258a7e87c58b9febe80
Partik SIngh <partiksingh@pop-os.localdomain>
2022-10-16 00:15:18 +0530

    Completed Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
-
+import { useState } from "react"
+import {AiOutlineMenu} from 'react-icons/ai'
 const Navbar = () => {
+    const [active, setActive] = useState(false)
   return (
     <>
     <nav className="navbar bg-white w-full px-10 h-20 items-center justify-between flex flex-column">
@@ -25,8 +27,11 @@ const Navbar = () => {
             <button><Link href="signup">Sign Up</Link></button>
             <button><Link href="login">Log In</Link></button>
         </div>
+        <div className="hidden md_max:block">
+            <AiOutlineMenu fontSize="2rem" onClick={()=>{setActive(!active)}}/>
+        </div>
     </nav>
-     <div className='hidden md_max:flex bg-light_background flex-col justify-center items-center text-center py-6'>
+     <div className={`nav_box ${active ? "active" :""} hidden md_max:flex bg-light_background flex-col justify-center items-center text-center py-6`}  >
         <div className="text-lg font-bold w-full">
           <div className="border-b-2 my-2 border-dark_background w-full hover:text-dark_heading"><Link href="/"><a>Home</a></Link></div>
           <div className="border-b-2 my-2 border-dark_background hover:text-dark_heading"><Link href="/billing">Billing</Link></div>
