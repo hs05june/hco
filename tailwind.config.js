bdb20ce86809ace235488e8012e191b85b7eb6c4
hs05june <harpreet.singh.cse21@itbhu.ac.in>
2022-10-15 19:51:52 +0530

    Add Item Popup

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,18 @@ module.exports = {
       dark_heading:'#1515BF',
       light_heading:'#3E3ED5',
       moderate_color:'#6A6ADC',
+      light_color:'#DBEBFA',
       dark_background:'#8989EB',
       light_background:'#C6C6FB',
       white:colors.white,
       black:colors.black,
+      red:colors.red,
       button_color:'#5694FE',
       delete:'#F15152',
       edit:'#1789FC',
-      card_border:'#FEF9FF'  
+      card_border:'#FEF9FF',
+      card:'#E4E4E4',
+      blue:'#3b2cdd'
     },
     screens:{
       "sm_max":{"max":"768px"},
